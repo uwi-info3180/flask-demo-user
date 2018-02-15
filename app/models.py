0cb2942b0e21a029f9fd2f4ef028aba7691f4849
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2018-02-15 00:29:26 -0500

    Change table name to a plural table name

--- app/models.py
+++ app/models.py
@@ -3,6 +3,12 @@ from werkzeug.security import generate_password_hash
 
 
 class UserProfile(db.Model):
+    # You can use this to change the table name. The default convention is to use
+    # the class name. In this case a class name of UserProfile would create a
+    # user_profile (singular) table, but if we specify __tablename__ we can change it
+    # to `user_profiles` or some other name.
+    __tablename__ = 'user_profiles'
+
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
