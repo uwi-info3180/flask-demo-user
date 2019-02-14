47e0ddf4b51322403df354ce0dd4cbb79fa72939
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2019-02-13 22:55:09 -0500

    Update module/package reference for Python 3

--- app/views.py
+++ app/views.py
@@ -8,8 +8,8 @@ This file creates your application.
 from app import app, db, login_manager
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
-from forms import LoginForm
-from models import UserProfile
+from app.forms import LoginForm
+from app.models import UserProfile
 from werkzeug.security import check_password_hash
 
 ###
