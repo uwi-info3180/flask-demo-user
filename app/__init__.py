83b5f572bc0b094195cb13739bbdd0a2612a0fad
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2022-02-16 22:53:52 -0500

    Update application to use config file and add migrate commands to init
    Separate scripts file for migrate commands no longer needed.
    We will use flask cli instead

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from .config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "this is a super secure key"  # you should make this more random and unique
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://user:password@localhost/database"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # added just to suppress a warning
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Flask-Login login manager
 login_manager = LoginManager()
@@ -15,5 +16,4 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'  # necessary to tell Flask-Login what the default route is for the login page
 login_manager.login_message_category = "info"  # customize the flash message category
 
-app.config.from_object(__name__)
 from app import views
